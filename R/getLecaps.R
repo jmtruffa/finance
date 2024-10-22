08145e9b753b07c5cdfefd95aba475f82a5dda29
juan <>
2024-10-22 07:06:02 -0300

    Simplificada getlecaps trae todo el df de la base ya que con dos fechas muy separadas no era conveniente lo que devolvía por la vigencia

--- R/getLecaps.R
+++ R/getLecaps.R
@@ -1,17 +1,16 @@
 #' @title getLecaps
-#' @description Devuelve las Lecaps vigentes segun fecha provista
-#' @param date Fecha en formato "YYYY-MM-DD". Por defecto la fecha actual.
-#' @param ... Parametros de conexion a la base de datos
-#' @return Vector con los tickers de las Lecaps vigentes
+#' @description Devuelve las Lecaps
+#'
+#' @return Vector con los tickers de las Lecaps vigentes y resto de info.
 #' @examples
-#' getLecaps("2024-08-10")
 #' getLecaps()
 #' @export
 #' @importFrom functions dbExecuteQuery
 #'
-getLecaps = function(date = Sys.Date(), ...) {
+getLecaps = function(...) {
   require(functions)
-  query = paste0("SELECT ticker FROM lecaps WHERE date_vto >= '",date, "' AND date_liq <= '",date, "'")
+  #query = paste0("SELECT ticker FROM lecaps WHERE date_vto >= '",date, "' AND date_liq <= '",date, "'")
+  query = paste0("SELECT * FROM lecaps")
   functions::dbExecuteQuery(query, ...)
 }
 
