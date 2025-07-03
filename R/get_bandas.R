3f615b3ef33f26109f6e3a2b12864cbf8faa0756
juan <>
2025-07-03 15:40:39 -0300

    quité feriados y calcula continuo salvo sabado y domingo.

--- R/get_bandas.R
+++ R/get_bandas.R
@@ -2,18 +2,18 @@
 #' @description Devuelve las bandas superior e inferior según la "FASE 3" del programa monetario del gobierno Milei. Comienza el 14-abr-25 con 1000 y 1400. Ajusta al 1% mensual
 #' @return Un data frame con las bandas superior e inferior y la fecha.
 #' @export
-get_bandas = function() {
+get_bandas = function(end_date = "2027-12-31") {
 library(bizdays)
 library(lubridate)
-library(functions)
+#library(functions)
 
 cal <- create.calendar(
   name      = "cal",
-  holidays  = functions::getFeriados(server = server, port = port),
+  #holidays  = functions::getFeriados(server = server, port = port),
   weekdays  = c("saturday", "sunday")
 )
 start_date <- as.Date("2025-04-14")   # donde arrancan las bandas
-end_date   <- as.Date("2027-12-31")
+end_date   <- as.Date(end_date)
 
 dates <- bizseq(start_date, end_date, cal = "cal")   # vector Date
 meses_dec <- time_length(interval(start_date, dates), unit = "months")
